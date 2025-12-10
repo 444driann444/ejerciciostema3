cb639a2790f7460e45411ef6dd0eff25be15c2e5
TGA <TGA@D13-EQ04.iesam.net>
2025-12-10 09:21:57 +0100

    ejercicios retocados

--- src/Ejerciciostema3/EJERCICIO9PDF.java
+++ src/Ejerciciostema3/EJERCICIO9PDF.java
@@ -20,7 +20,7 @@ public class EJERCICIO9PDF {
 		
 		do {
 			int numeroran = (int) (Math.random()*100) ;
-			System.out.println("El numero aleatorio es " + numeroran);
+			
 			
 			
 
@@ -31,19 +31,43 @@ public class EJERCICIO9PDF {
 			contadorturn++;
 			
 			
-			
 			System.out.println(j1.getNombre1() + " Introduce un numero" );
 			int numerojugador1 = teclado.nextInt();
 			
 			System.out.println(j2.getNombre1() + " Introduce un numero");
 			int numerojugador2 = teclado.nextInt();
 			
+			System.out.println("El numero aleatorio es " + numeroran);
 			
+			int distanciajugador1;
+			int distanciajugador2;
+			
+			distanciajugador1 = Math.abs(numeroran - numerojugador1);
+			distanciajugador2 = Math.abs(numeroran - numerojugador2);
+			
+			if (distanciajugador1 < distanciajugador2) {
+				j2.reducirVida();
+				System.out.println(nombrejugador2  + " pierde 10 puntos de vida ");
+			}else if (distanciajugador2 < distanciajugador1) {
+				j1.reducirVida();
+				System.out.println(nombrejugador1  + " pierde 10 puntos de vida ");
+			}else {
+				System.out.println("Son iguales no se quitan vida");
+			}
+				System.out.println();
+				j1.imprimirVidaRestante();
+				j2.imprimirVidaRestante();
 			
 			
 			
 		}while(j1.haPerdido()|| j2.haPerdido());
 		teclado.close();
+		
+		if (j1.haPerdido()  == true) {
+			System.out.println("Ha ganado " + nombrejugador2);
+		}else if (j2.haPerdido() == true) {
+			System.out.println("Ha ganado " + nombrejugador1);
+		}
 	}
 
 }
