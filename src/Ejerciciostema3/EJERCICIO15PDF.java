58fc80c525624b3a0756796b2a48d4b7f8122d6b
444driann444 <adriantorresgarcia71@gmail.com>
2026-01-09 09:55:34 +0100

    eliminacion

--- src/Ejerciciostema3/EJERCICIO15PDF.java
+++ src/Ejerciciostema3/EJERCICIO15PDF.java
@@ -6,8 +6,5 @@ public class EJERCICIO15PDF {
 
 	public static void main(String[] args) {
 		
-	Scanner teclado = new Scanner(System.in);
-	System.out.println("Escribe lo que quieras");
-	int num = teclado.nextInt();
 	}
 }
